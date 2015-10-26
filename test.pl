8f4b7e4d3c8f12bc42173716c29915366daaf8cd
marcin <marbogster@gmail.com>
2015-10-26 20:49:00 +0100

    Some more code
    Change-Id: Ia7020d7ee517c0bcfc2f1a8b3e3972438f32a7ac

--- test.pl
+++ test.pl
@@ -1,8 +1,6 @@
-:- module(se2,[wykonaj]).
+:- dynamic([xpytaj_o_czytanie/1]).
 
-:- dynamic([xpozytywne/2, xnegatywne/2]).
-
-% hipotesa : lekka pozycja fantasy
+% hipoteza : lekka pozycja fantasy
 polec(lekka_pozycja_fantasy) :-
 	lubi(fantastyka),
 	czyta(rzadko).
@@ -11,11 +9,20 @@ polec(lekka_pozycja_fantasy) :-
 czyta(rzadko) :-
 	pytaj_o_czytanie(tramwaj).
 	
+czyta(rzadko) :-
+	pytaj_o_czytanie(zajecia).
+	
 % pytania o czytanie
+
 pytaj_o_czytanie(X) :-
-	!, write(' Gdzie najczesciej czytasz (Dom,Tramwaj,Miedzy zajeciami)\n'),
+	xpytaj_o_czytanie(X),!.
+	
+pytaj_o_czytanie(X) :-
+	not(xpytaj_o_czytanie(X)),
+	!, write(' Gdzie najczesciej czytasz (Dom,Tramwaj,Zajecia)\n'),
 	readln([Replay]),
-	odpowiedz_o_czytaniu(Replay, X).
+	odpowiedz_o_czytaniu(Replay, X),
+	assertz(xpytaj_o_czytanie(X)).
 	
 odpowiedz_o_czytaniu(Replay, dom):-
 	sub_string(Replay, 0, _, _, 'Dom').
@@ -24,7 +31,7 @@ odpowiedz_o_czytaniu(Replay, tramwaj):-
 	sub_string(Replay, 0, _, _, 'Tramwaj').
 	
 odpowiedz_o_czytaniu(Replay, zajecia):-
-	sub_string(Replay, 0, _, _, 'Miedzy zajeciami').
+	sub_string(Replay, 0, _, _, 'Zajecia').
 	 
 % fakt posredni : lubi fantastyke
 lubi(fantastyka) :-
