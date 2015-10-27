469d61ea0cef0eace98ff3ee975355d52a4647dd
marcin <marbogster@gmail.com>
2015-10-27 11:34:47 +0100

    Improved saving answers. Modified reading question.
    Change-Id: Ifaa09bf202a0b81e41a0ee0a10a5bee4cb021653

--- test.pl
+++ test.pl
@@ -1,85 +1,75 @@
-:- dynamic([xpytaj_o_czytanie/1]).
+:- dynamic([memory/2]).
 
-% hipoteza : lekka pozycja fantasy
+% Hipotezy
 polec(lekka_pozycja_fantasy) :-
-	lubi(fantastyka),
-	czyta(rzadko).
-	
-% fakt posredni : czyta rzadko
+    lubi(fantastyka),
+    czyta(rzadko).
+
+    
+% Fakty posrednie
 czyta(rzadko) :-
-	pytaj_o_czytanie(tramwaj).
-	
+    pytaj_o_czytanie(tramwaj).
+    
 czyta(rzadko) :-
-	pytaj_o_czytanie(zajecia).
-	
-% pytania o czytanie
+    pytaj_o_czytanie(zajecia).
 
-pytaj_o_czytanie(X) :-
-	xpytaj_o_czytanie(X),!.
-	
-pytaj_o_czytanie(X) :-
-	[F,_] = difference([dom,tramwaj,zajeca],X),
-	xpytaj_o_czytanie(F),!.
-	
-pytaj_o_czytanie(X) :-
-	[_,L] = difference([dom,tramwaj,zajeca],X),
-	xpytaj_o_czytanie(L),!.
-	
-
-pytaj_o_czytanie(X) :-
-	!,not(xpytaj_o_czytanie(X)),
-	write(' Gdzie najczesciej czytasz (dom,tramwaj,zajecia)\n'),
-	readln([Replay]),
-	odpowiedz_o_czytaniu(Replay,X). 
-	
-odpowiedz_o_czytaniu(Replay,X):-
-	sub_string(Replay, 0, _, _, 'dom'),
-	pamietaj(dom),
-	X == dom.
-
-odpowiedz_o_czytaniu(Replay,X):-
-	sub_string(Replay, 0, _, _, 'tramwaj'),
-	pamietaj(tramwaj),
-	X == tramwaj.
-	
-odpowiedz_o_czytaniu(Replay,X):-
-	sub_string(Replay, 0, _, _, 'zajecia'),
-	pamietaj(zajecia),
-	X == zajecia.
-	
-pamietaj(X) :-
-	assertz(xpytaj_o_czytanie(X)).
-	 
-% fakt posredni : lubi fantastyke
 lubi(fantastyka) :-
-	pytaj_o_gre(rpg).
-	
+    pytaj_o_gre(rpg).
+
+
+% Pytania o symptomy
+pytaj_o_czytanie(X) :-
+    memory(czytanie,X).
+
+pytaj_o_czytanie(X) :-
+    subtract([dom,tramwaj,zajecia],[X],L),
+    nth1(1,L,F),
+    nth1(2,L,S),
+    not(memory(czytanie, F)),
+    not(memory(czytanie, S)),
+    write(' Gdzie najczesciej czytasz (dom,tramwaj,zajecia)\n'),
+    readln([Replay]),
+    pamietaj(czytanie, Replay),
+    Replay == X,
+    odpowiedz_o_czytaniu(X).
+
+odpowiedz_o_czytaniu(dom):- true.
+
+odpowiedz_o_czytaniu(tramwaj):- true.
+    
+odpowiedz_o_czytaniu(zajecia):- true.
+        
 % pytania o to w co gra
 pytaj_o_gre(X) :-
-	!, write(' W jakie gry komputerowe najczesciej grasz? (Shootery,RPG,Strategie)\n'),
-	readln([Replay]),
-	odpowiedz_o_grze(Replay, X).
-	
+    !, write(' W jakie gry komputerowe najczesciej grasz? (Shootery,RPG,Strategie)\n'),
+    readln([Replay]),
+    odpowiedz_o_grze(Replay, X).
+    
 odpowiedz_o_grze(Replay, rpg):-
-	sub_string(Replay, 0, _, _, 'RPG').
+    sub_string(Replay, 0, _, _, 'RPG').
 
 odpowiedz_o_grze(Replay, shootery):-
-	sub_string(Replay, 0, _, _, 'Shootery').
-	
+    sub_string(Replay, 0, _, _, 'Shootery').
+    
 odpowiedz_o_grze(Replay, strategie):-
-	sub_string(Replay, 0, _, _, 'Strategie').
-	
+    sub_string(Replay, 0, _, _, 'Strategie').
+    
 
-% czyszczenie zapisanych danych
+% Funkcje pomocnicze
+pamietaj(X, Y) :-
+    assertz(memory(X, Y)).
+
+
+% Czyszczenie zapisanych danych
 wyczysc_fakty :-
-	write('\nNacisnij enter aby zakonczyc\n'),
-	retractall(xpytaj_o_czytanie(_)),
-	readln(_).
-	
-% do odpalania
+    write('Nacisnij enter aby zakonczyc\n'),
+    retractall(memory(_,_)),
+    readln(_).
+    
+% G³ówna funkcja
 wykonaj_ksiazki :-
-	polec(X),!,write('Polecam Ci '), write(X), nl, wyczysc_fakty.
-
-
-
-	
+    polec(X),!,write('Polecam Ci '), write(X), nl, wyczysc_fakty.
+    
+wykonaj_ksiazki :-
+    write('\nNie znalazlem dopasowania'),nl,
+    wyczysc_fakty.
