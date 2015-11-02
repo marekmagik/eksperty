e944297fb0b99c58c4e7dee7811b1315e5c0da01
marcin <marbogster@gmail.com>
2015-11-02 21:20:32 +0100

    Definitions of questions contains possible answers
    Change-Id: I217e486759a009fe02373ade6e60b0c02f39239d

--- test.pl
+++ test.pl
@@ -1,26 +1,26 @@
 :- dynamic([memory/2]).
 
 % Pytania
-question(Text, 1) :- Text= 'Jak czêsto czytasz ksi¹¿ki '.
-question(Text, 2) :- Text= 'Gdzie zazwyczaj czytasz ksi¹¿ki '.
-question(Text, 3) :- Text= 'Który przymiotnik lepiej Ciê opisuje '.
-question(Text, 4) :- Text= 'Uwa¿asz, ¿e rozwiniêta wyobraŸnia jest istotna w ¿yciu codziennym '.
-question(Text, 5) :- Text= 'Czy lubisz podró¿e i odkrywanie nowych miejsc '.
-question(Text, 6) :- Text= 'Szklanka jest do po³owy …. '.
-question(Text, 7) :- Text= 'Czy interesujesz siê faktami i zdarzeniami historycznymi '.
-question(Text, 8) :- Text= 'Czy wierzysz, ¿e poza Ziemi¹ istnieje ¿ycie '.
-question(Text, 9) :- Text= 'Które okreœlenie najlepiej do Ciebie pasuje '.
-question(Text, 10) :- Text= 'Czy interesujesz siê odkryciami naukowymi i zdobywaniem wiedzy '.
-question(Text, 11) :- Text= 'W jakie gry komputerowe najczesciej grasz '.
-question(Text, 12) :- Text= 'Czy interesujesz siê intrygami i ³amig³ówkami '.
-question(Text, 13) :- Text= 'Jakie filmy czêœciej ogl¹dasz w kinie '.
-question(Text, 14) :- Text= 'Na wakacje najchêtniej pojecha³byœ: '.
-question(Text, 15) :- Text= 'Wolisz ogl¹daæ '.
-question(Text, 16) :- Text= 'Widzisz na ulicy bezdomnego zbieraj¹cego na jedzenie. Co robisz: '.
-question(Text, 17) :- Text= 'Który kierunek studiów by³by dla Ciebie najbardziej interesuj¹cy '.
-question(Text, 18) :- Text= 'Swoj¹ drug¹ po³ówkê w dniu jego/jej urodzin zabra³byœ: '.
-question(Text, 19) :- Text= 'Któr¹ z tych mocy byæ wybra³ gdybyœ móg³ '.
-question(Text, 20) :- Text= 'Czy posiadasz swoj¹ ulubion¹ postaæ historyczn¹, o której dyskutowa³eœ ze znajomym '.
+question(Text, Answers, 1) :- Text= 'Jak czêsto czytasz ksi¹¿ki ', Answers = [].
+question(Text, Answers, 2) :- Text= 'Gdzie zazwyczaj czytasz ksi¹¿ki ', Answers = [dom,tramwaj,zajecia].
+question(Text, Answers, 3) :- Text= 'Który przymiotnik lepiej Ciê opisuje ', Answers = [].
+question(Text, Answers, 4) :- Text= 'Uwa¿asz, ¿e rozwiniêta wyobraŸnia jest istotna w ¿yciu codziennym ', Answers = [].
+question(Text, Answers, 5) :- Text= 'Czy lubisz podró¿e i odkrywanie nowych miejsc ', Answers = [].
+question(Text, Answers, 6) :- Text= 'Szklanka jest do po³owy …. ', Answers = [].
+question(Text, Answers, 7) :- Text= 'Czy interesujesz siê faktami i zdarzeniami historycznymi ', Answers = [].
+question(Text, Answers, 8) :- Text= 'Czy wierzysz, ¿e poza Ziemi¹ istnieje ¿ycie ', Answers = [].
+question(Text, Answers, 9) :- Text= 'Które okreœlenie najlepiej do Ciebie pasuje ', Answers = [].
+question(Text, Answers, 10) :- Text= 'Czy interesujesz siê odkryciami naukowymi i zdobywaniem wiedzy ', Answers = [].
+question(Text, Answers, 11) :- Text= 'W jakie gry komputerowe najczesciej grasz ', Answers = [shootery,rpg,strategie].
+question(Text, Answers, 12) :- Text= 'Czy interesujesz siê intrygami i ³amig³ówkami ', Answers = [tak,nie].
+question(Text, Answers, 13) :- Text= 'Jakie filmy czêœciej ogl¹dasz w kinie ', Answers = [].
+question(Text, Answers, 14) :- Text= 'Na wakacje najchêtniej pojecha³byœ: ', Answers = [].
+question(Text, Answers, 15) :- Text= 'Wolisz ogl¹daæ ', Answers = [].
+question(Text, Answers, 16) :- Text= 'Widzisz na ulicy bezdomnego zbieraj¹cego na jedzenie. Co robisz: ', Answers = [].
+question(Text, Answers, 17) :- Text= 'Który kierunek studiów by³by dla Ciebie najbardziej interesuj¹cy ', Answers = [].
+question(Text, Answers, 18) :- Text= 'Swoj¹ drug¹ po³ówkê w dniu jego/jej urodzin zabra³byœ: ', Answers = [].
+question(Text, Answers, 19) :- Text= 'Któr¹ z tych mocy byæ wybra³ gdybyœ móg³ ', Answers = [].
+question(Text, Answers, 20) :- Text= 'Czy posiadasz swoj¹ ulubion¹ postaæ historyczn¹, o której dyskutowa³eœ ze znajomym ', Answers = [].
 
 
 % Hipotezy
@@ -32,19 +32,28 @@ polec(ciezka_pozycja_fantasy) :-
     lubi(fantastyka),
     czyta(czesto).
     
+polec(kryminal) :-
+	lubi(zagadki).
+	
+    
     
 % Fakty posrednie
 czyta(rzadko) :-
-    question(Q,2),
-    pytaj(Q,[dom,tramwaj,zajecia],[tramwaj,zajecia],czytanie).
+    question(Q,A,2),
+    pytaj(Q,A,[tramwaj,zajecia],czytanie).
 
 czyta(czesto) :-
-    question(Q,2),
-    pytaj(Q,[dom,tramwaj,zajecia],[dom],czytanie).
+    question(Q,A,2),
+    pytaj(Q,A,[dom],czytanie).
 
 lubi(fantastyka) :-
-    question(Q,11),
-    pytaj(Q,[shootery,rpg,strategie],[rpg],granie).
+    question(Q,A,11),
+    pytaj(Q,A,[rpg],granie).
+    
+lubi(zagadki) :-
+	question(Q,A,12),
+	pytaj(Q,A,[tak],zagadki).
+	
         
 
 % Funkcje pomocnicze
