f8a8c5e50abe28797b9beb8f8f9d4bc9602b00a7
marcin <marbogster@gmail.com>
2015-11-08 13:01:50 +0100

    Refactoring
    Change-Id: Iedde729b349516e1908e98cfbd9a5c0b9de18767

--- test.pl
+++ test.pl
@@ -1,26 +1,26 @@
 :- dynamic([memory/2]).
 
 % Pytania
-question(Text, Answers, 1) :- Text= 'Jak czêsto czytasz ksi¹¿ki ', Answers = [codziennie,okazjonalnie,rzadko].
-question(Text, Answers, 2) :- Text= 'Gdzie zazwyczaj czytasz ksi¹¿ki ', Answers = [dom,tramwaj,zajecia].
-question(Text, Answers, 3) :- Text= 'Który przymiotnik lepiej Ciê opisuje ', Answers = [opanowany,impulsywny].
-question(Text, Answers, 4) :- Text= 'Uwa¿asz, ¿e rozwiniêta wyobraŸnia jest istotna w ¿yciu codziennym ', Answers = [tak,nie].
-question(Text, Answers, 5) :- Text= 'Czy lubisz podró¿e i odkrywanie nowych miejsc ', Answers = [tak,nie].
-question(Text, Answers, 6) :- Text= 'Szklanka jest do po³owy …. ', Answers = [pelna,pusta].
-question(Text, Answers, 7) :- Text= 'Czy interesujesz siê faktami i zdarzeniami historycznymi ', Answers = [tak,nie].
-question(Text, Answers, 8) :- Text= 'Czy wierzysz, ¿e poza Ziemi¹ istnieje ¿ycie ', Answers = [tak,nie].
-question(Text, Answers, 9) :- Text= 'Które okreœlenie najlepiej do Ciebie pasuje ', Answers = [introwertyk,ekstrawertyk,melancholik].
-question(Text, Answers, 10) :- Text= 'Czy interesujesz siê odkryciami naukowymi i zdobywaniem wiedzy ', Answers = [tak,nie].
-question(Text, Answers, 11) :- Text= 'W jakie gry komputerowe najczesciej grasz ', Answers = [shootery,rpg,strategie,przygodowe,horrory].
-question(Text, Answers, 12) :- Text= 'Czy interesujesz siê intrygami i ³amig³ówkami ', Answers = [tak,nie].
-question(Text, Answers, 13) :- Text= 'Jakie filmy czêœciej ogl¹dasz w kinie ', Answers = [akcji,familijne,psychologiczne,przygodowe,basnie,thrillery,dramaty].
-question(Text, Answers, 14) :- Text= 'Na wakacje najchêtniej pojecha³byœ: ', Answers = [gory,morze,zwiedzanie_zabytkow,kamping].
-question(Text, Answers, 15) :- Text= 'Wolisz ogl¹daæ ', Answers = [].
-question(Text, Answers, 16) :- Text= 'Widzisz na ulicy bezdomnego zbieraj¹cego na jedzenie. Co robisz: ', Answers = [].
-question(Text, Answers, 17) :- Text= 'Który kierunek studiów by³by dla Ciebie najbardziej interesuj¹cy ', Answers = [informatyka,fizyka,historia,prawo,kryminalistyka].
-question(Text, Answers, 18) :- Text= 'Swoj¹ drug¹ po³ówkê w dniu jego/jej urodzin zabra³byœ: ', Answers = [kolacja,film_w_domu,spotkanie_z_przyjaciolmi].
-question(Text, Answers, 19) :- Text= 'Któr¹ z tych mocy byæ wybra³ gdybyœ móg³ ', Answers = [].
-question(Text, Answers, 20) :- Text= 'Czy posiadasz swoj¹ ulubion¹ postaæ historyczn¹, o której dyskutowa³eœ ze znajomym ', Answers = [tak,nie].
+question(Text, Answers, jak_czesto) :- Text= 'Jak czêsto czytasz ksi¹¿ki ', Answers = [codziennie,okazjonalnie,rzadko].
+question(Text, Answers, gdzie) :- Text= 'Gdzie zazwyczaj czytasz ksi¹¿ki ', Answers = [dom,tramwaj,zajecia].
+question(Text, Answers, jest) :- Text= 'Który przymiotnik lepiej Ciê opisuje ', Answers = [opanowany,impulsywny].
+question(Text, Answers, wyobraznia) :- Text= 'Uwa¿asz, ¿e rozwiniêta wyobraŸnia jest istotna w ¿yciu codziennym ', Answers = [tak,nie].
+question(Text, Answers, podroze) :- Text= 'Czy lubisz podró¿e i odkrywanie nowych miejsc ', Answers = [tak,nie].
+question(Text, Answers, szklanka) :- Text= 'Szklanka jest do po³owy …. ', Answers = [pelna,pusta].
+question(Text, Answers, historia) :- Text= 'Czy interesujesz siê faktami i zdarzeniami historycznymi ', Answers = [tak,nie].
+question(Text, Answers, zycie_pozaziemskie) :- Text= 'Czy wierzysz, ¿e poza Ziemi¹ istnieje ¿ycie ', Answers = [tak,nie].
+question(Text, Answers, charakter) :- Text= 'Które okreœlenie najlepiej do Ciebie pasuje ', Answers = [introwertyk,ekstrawertyk,melancholik].
+question(Text, Answers, nauka) :- Text= 'Czy interesujesz siê odkryciami naukowymi i zdobywaniem wiedzy ', Answers = [tak,nie].
+question(Text, Answers, gry_komputerowe) :- Text= 'W jakie gry komputerowe najczesciej grasz ', Answers = [shootery,rpg,strategie,przygodowe,horrory].
+question(Text, Answers, zagadki) :- Text= 'Czy interesujesz siê intrygami i ³amig³ówkami ', Answers = [tak,nie].
+question(Text, Answers, filmy) :- Text= 'Jakie filmy czêœciej ogl¹dasz w kinie ', Answers = [akcji,familijne,psychologiczne,przygodowe,basnie,thrillery,dramaty].
+question(Text, Answers, wakacje) :- Text= 'Na wakacje najchêtniej pojecha³byœ: ', Answers = [gory,morze,zwiedzanie_zabytkow,kamping].
+question(Text, Answers, oglada) :- Text= 'Wolisz ogl¹daæ ', Answers = [].
+question(Text, Answers, bezdomny) :- Text= 'Widzisz na ulicy bezdomnego zbieraj¹cego na jedzenie. Co robisz: ', Answers = [].
+question(Text, Answers, studia) :- Text= 'Który kierunek studiów by³by dla Ciebie najbardziej interesuj¹cy ', Answers = [informatyka,fizyka,historia,prawo,kryminalistyka].
+question(Text, Answers, urodziny_partnera) :- Text= 'Swoj¹ drug¹ po³ówkê w dniu jego/jej urodzin zabra³byœ: ', Answers = [kolacja,film_w_domu,spotkanie_z_przyjaciolmi].
+question(Text, Answers, moc) :- Text= 'Któr¹ z tych mocy byæ wybra³ gdybyœ móg³ ', Answers = [].
+question(Text, Answers, postac_historyczna) :- Text= 'Czy posiadasz swoj¹ ulubion¹ postaæ historyczn¹, o której dyskutowa³eœ ze znajomym ', Answers = [tak,nie].
 
 
 % Hipotezy
@@ -74,36 +74,26 @@ polec(science_fiction) :-
     
 % Fakty posrednie
 czyta(rzadko) :-
-    question(Q,A,2),
-    pytaj(Q,A,[tramwaj,zajecia],2).
+    pytaj([tramwaj,zajecia],gdzie).
 
 czyta(czesto) :-
-    question(Q,A,2),
-    pytaj(Q,A,[dom],2).
+    pytaj([dom],gdzie).
 
 lubi(fantastyka) :-
-    question(Q1,A1,11),
-    pytaj(Q1,A1,[rpg],11),
-    (question(Q2,A2,4),
-    pytaj(Q2,A2,[tak],4);
-    question(Q3,A3,8),
-    pytaj(Q3,A3,[tak],8)),
-    question(Q4,A4,13),
-    pytaj(Q4,A4,[przygodowe,basnie],13).
+    pytaj([rpg],gry_komputerowe),
+    (pytaj([tak],wyobraznia);
+    pytaj([tak],zycie_pozaziemskie)),
+    pytaj([przygodowe,basnie],filmy).
     
     
 lubi(zagadki) :-
-	question(Q1,A1,12),
-	pytaj(Q1,A1,[tak],12),
+	pytaj([tak],zagadki),
 	lubi_grac_w(przygodowe),
-	question(Q2,A2,17),
-	pytaj(Q2,A2,[kryminalistyka],17).
+	pytaj([kryminalistyka],studia).
 	
 lubi(podroze) :-
-	question(Q1,A1,5),
-	pytaj(Q1,A1,[tak],5),
-	question(Q2, A2, 14),
-	pytaj(Q2,A2,[gory],14).
+	pytaj([tak],podroze),
+	pytaj([gory],wakacje).
 	
 lubi(horrory) :-
 	lubi_grac_w(horrory),
@@ -111,80 +101,55 @@ lubi(horrory) :-
 		
 lubi(biografia) :-
 	lubi(historia),
-	question(Q1, A1, 20),
-	pytaj(Q1,A1,[tak],20).
+	pytaj([tak],postac_historyczna).
 
 
 lubi(lekkie_pozycje) :-
-    (question(Q1,A1,1),
-    pytaj(Q1,A1,[okazjonalnie,rzadko],1);
+    (pytaj([okazjonalnie,rzadko],jak_czesto);
     czyta(rzadko)),
-    question(Q2,A2,3),
-    pytaj(Q2,A2,[impulsywny],3),
-    (question(Q3,A3,5),
-    pytaj(Q3,A3,[tak],5);
-    question(Q4,A4,13),
-    pytaj(Q4,A4,[przygodowe,akcji,familijne],13)).
+    pytaj([impulsywny],jest),
+    (pytaj([tak],podroze);
+    pytaj([przygodowe,akcji,familijne],filmy)).
         
     
 lubi(ciezkie_pozycje) :-
-    (question(Q1,A1,1),
-    pytaj(Q1,A1,[codziennie],1);
+    (pytaj([codziennie],jak_czesto);
     czyta(czesto)),
-    (question(Q2,A2,4),
-    pytaj(Q2,A2,[tak],4);
-    question(Q3,A3,13),
-    pytaj(Q3,A3,[psychologiczne,thrillery,dramaty],13)),
-    question(Q4,A4,3),
-    pytaj(Q4,A4,[opanowany],3).
+    (pytaj([tak],wyobraznia);
+    pytaj([psychologiczne,thrillery,dramaty],filmy)),
+    pytaj([opanowany],jest).
 
 lubi(naukowe) :-
-    question(Q1,A1,17),
-    pytaj(Q1,A1,[informatyka,fizyka],17),
-    question(Q2,A2,10),
-    pytaj(Q2,A2,[tak],10),
-    question(Q3,A3,8),
-    pytaj(Q3,A3,[tak],8).
+    pytaj([informatyka,fizyka],studia),
+    pytaj([tak],nauka),
+    pytaj([tak],zycie_pozaziemskie).
 % // TODO: dodaæ strategie
 
         	
 lubi(romans) :-
-    question(Q1,A1,18),
-    pytaj(Q1,A1,[kolacja,film_w_domu],18),
-    question(Q2,A2,11),
-    pytaj(Q2,A2,[rpg,przygodowe],11),
-    question(Q3,A3,9),
-    pytaj(Q3,A3,[introwertyk,melancholik],9),
-    question(Q4,A4,6),
-    pytaj(Q4,A4,[pelna],6).
+    pytaj([kolacja,film_w_domu],urodziny_partnera),
+    pytaj([rpg,przygodowe],gry_komputerowe),
+    pytaj([introwertyk,melancholik],charakter),
+    pytaj([pelna],szklanka).
 
 lubi(historia) :-
-    (question(Q1,A1,20),
-    pytaj(Q1,A1,[tak],20),
-    question(Q2,A2,11),
-    pytaj(Q2,A2,[shootery,strategie],11),
-    question(Q3,A3,14),
-    pytaj(Q3,A3,[gory,zwiedzanie_zabytkow],14));
-    question(Q4,A4,7),
-    pytaj(Q4,A4,[tak],7),
-    question(Q5,A5,17),
-    pytaj(Q5,A5,[historia],17).        
+    (pytaj([tak],postac_historyczna),
+    pytaj([shootery,strategie],gry_komputerowe),
+    pytaj([gory,zwiedzanie_zabytkow],wakacje));
+    pytaj([tak],historia),
+    pytaj([historia],studia).        
 
 lubi(podroze) :-
-    question(Q,A,5),
-    pytaj(Q,A,[tak],5).
+    pytaj([tak],podroze).
 	
 typ(optymista) :-
-	question(Q, A, 6),
-	pytaj(Q,A,[pelna],6).
+	pytaj([pelna],szklanka).
 	
 typ(pesymista) :-
-	question(Q, A, 6),
-	pytaj(Q,A,[pusta],6).
+	pytaj([pusta],szklanka).
 	
 lubi_grac_w(X) :-
-    question(Q,A,11),
-    pytaj(Q,A,[X],11).
+    pytaj([X],gry_komputerowe).
     
 
 % Funkcje pomocnicze
@@ -194,11 +159,11 @@ pamietaj(X, Y) :-
 sprawdz([],_) :- false.    
 sprawdz([X|Rest],Type) :- memory(Type,X);sprawdz(Rest,Type).
 
-pytaj(_,_,Answer,Type) :-
-	memory(Type,Answer).
+pytaj(Answer,Type) :-
+	sprawdz(Answer,Type).
 	
-pytaj(Question,Answers,Answer,Type) :-
-	(sprawdz(Answer,Type));
+pytaj(Answer,Type) :-
+	question(Question,Answers,Type),
 	(not(sprawdz(Answers, Type)),
 	uzyskaj_poprawna_odpowiedz(Question,Answers,Reply),
 	pamietaj(Type,Reply),
