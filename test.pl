d2b2d187d4ce68dd65168a40f908bf7245cf7a8c
Karol Wójcik <karolw@student.agh.edu.pl>
2015-11-10 09:25:30 +0100

    stack prototype for why/how questions
    Change-Id: Iaa107c954bfc1f8c54c9307388e05657931dbc08

--- test.pl
+++ test.pl
@@ -25,187 +25,210 @@ question(Text, Answers, postac_historyczna) :- Text= 'Czy posiadasz swoj¹ ulubio
 
 % Hipotezy
 polec(lekka_pozycja_fantasy) :-
-    lubi(fantastyka),
-    lubi(lekkie_pozycje),
-    pamietaj(polec,lekka_pozycja_fantasy).
+    lubi(fantastyka, [polec(lekka_pozycja_fantasy)],Stack1),
+%    nl,nl,write('FANTASTYKA OK:  '),write(Stack1),nl,nl,
+    lubi(lekkie_pozycje, [polec(lekka_pozycja_fantasy)],Stack2),
+%    nl,nl,write('FANTASTYKA OK:  '),write(Stack2),nl,nl,
+    pamietaj(polec,lekka_pozycja_fantasy,ResultStack).
 
 polec(ciezka_pozycja_fantasy) :-
-    lubi(fantastyka),
-    lubi(ciezkie_pozycje),
-    pamietaj(polec,ciezka_pozycja_fantasy).
+    lubi(fantastyka,[polec(ciezka_pozycja_fantasy)],Stack1),
+    lubi(ciezkie_pozycje,[polec(ciezka_pozycja_fantasy)],Stack2),
+    pamietaj(polec,ciezka_pozycja_fantasy,ResultStack).
 
 polec(popularnonaukowa) :-
-    lubi(naukowe),
-    lubi(ciezkie_pozycje),
-    pamietaj(polec,popularnonaukowa).
+    lubi(naukowe,[polec(popularnonaukowa)],Stack1),
+    lubi(ciezkie_pozycje,[polec(popularnonaukowa)],Stack2),
+    pamietaj(polec,popularnonaukowa,ResultStack).
 	
 polec(romans) :-
-    lubi(romans),
-    lubi(lekkie_pozycje),
-    pamietaj(polec,romans).
+    lubi(romans,[polec(romans)],Stack1),
+    lubi(lekkie_pozycje,[polec(romans)],Stack2),
+    pamietaj(polec,romans,ResultStack).
     
 polec(ciezka_pozycja_historyczna) :-
-    lubi(historia),
-    lubi(ciezkie_pozycje),
-    pamietaj(polec,ciezka_pozycja_historyczna).
+    lubi(historia,[polec(ciezka_pozycja_historyczna)],Stack1),
+    lubi(ciezkie_pozycje,[polec(ciezka_pozycja_historyczna)],Stack2),
+    pamietaj(polec,ciezka_pozycja_historyczna,ResultStack).
 
 polec(lekka_pozycja_historyczna) :-
-    lubi(historia),
-    czyta(rzadko),
-    lubi(podroze),
-    pamietaj(polec,lekka_pozycja_historyczna).
+    lubi(historia,[polec(lekka_pozycja_historyczna)],Stack1),
+    czyta(rzadko,[polec(lekka_pozycja_historyczna)],Stack2),
+    lubi(podroze,[polec(lekka_pozycja_historyczna)],Stack3),
+    pamietaj(polec,lekka_pozycja_historyczna,ResultStack).
     
 polec(kryminal) :-
-	lubi(zagadki),
-	czyta(rzadko),
-	pamietaj(polec,kryminal).
+	lubi(zagadki,[polec(kryminal)],Stack1),
+	czyta(rzadko,[polec(kryminal)],Stack2),
+	pamietaj(polec,kryminal,ResultStack).
 	
 polec(ksiazka_przygodowa) :-
-	lubi(lekkie_pozycje),
-	lubi(podroze),
-	typ(optymista),
-	pamietaj(polec,ksiazka_przygodowa).
+	lubi(lekkie_pozycje,[polec(ksiazka_przygodowa)],Stack1),
+	lubi(podroze,[polec(ksiazka_przygodowa)],Stack2),
+	typ(optymista,[polec(ksiazka_przygodowa)],Stack3),
+	pamietaj(polec,ksiazka_przygodowa,ResultStack).
 	
 polec(horror) :-
-	lubi(horrory),
-	lubi(ciezkie_pozycje),
-	pamietaj(polec,horror).
+	lubi(horrory,[polec(horror)],Stack1),
+	lubi(ciezkie_pozycje,[polec(horror)],Stack2),
+	pamietaj(polec,horror,ResultStack).
 	
 polec(biografia) :-
-	lubi(biografia),
-	lubi(ciezkie_pozycje),
-	pamietaj(polec,biografia).
+	lubi(biografia,[polec(biografia)],Stack1),
+	lubi(ciezkie_pozycje,[polec(biografia)],Stack2),
+	pamietaj(polec,biografia,ResultStack).
 
 polec(science_fiction) :-
-	lubi(ciezkie_pozycje),
-	lubi(podroze),
-	lubi(naukowe),
-	pamietaj(polec,science_fiction).
+	lubi(ciezkie_pozycje,[polec(science_fiction)],Stack1),
+	lubi(podroze,[polec(science_fiction)],Stack2),
+	lubi(naukowe,[polec(science_fiction)],Stack3),
+	pamietaj(polec,science_fiction,ResultStack).
 	
     
 % Fakty posrednie
-czyta(rzadko) :-
-    pytaj([tramwaj,zajecia],gdzie),
-    pamietaj(czyta,rzadko).
+czyta(rzadko,InitialStack,ResultStack) :-
+    append(InitialStack,[czyta(rzadko)],Stack),
+    pytaj([tramwaj,zajecia],gdzie,Stack,ResultStack),
+    pamietaj(czyta,rzadko,ResultStack).
 
-czyta(czesto) :-
-    pytaj([dom],gdzie),
-    pamietaj(czyta,czesto).
+czyta(czesto,InitialStack,ResultStack) :-
+    append(InitialStack,[czyta(czesto)],Stack),
+    pytaj([dom],gdzie,Stack,ResultStack),
+    pamietaj(czyta,czesto,ResultStack).
 
-lubi(fantastyka) :-
-    pytaj([rpg],gry_komputerowe),
-    (pytaj([tak],wyobraznia);
-    pytaj([tak],zycie_pozaziemskie)),
-    pytaj([przygodowe,basnie],filmy),
-    pamietaj(lubi,fantastyka).
+lubi(fantastyka,InitialStack,ResultStack) :-
+    append(InitialStack,[lubi(fantastyka)],Stack),
+    pytaj([rpg],gry_komputerowe,Stack,Stack1),
+    (pytaj([tak],wyobraznia,Stack1,Stack2);
+    pytaj([tak],zycie_pozaziemskie,Stack1,Stack2)),
+    pytaj([przygodowe,basnie],filmy,Stack2,ResultStack),
+    pamietaj(lubi,fantastyka,ResultStack).
     
     
-lubi(zagadki) :-
-	pytaj([tak],zagadki),
-	lubi_grac_w(przygodowe),
-	pytaj([kryminalistyka],studia),
-	pytaj([czytanie_w_myslach,niewidzialnosc,podroz_w_czasie],moc),
-	pamietaj(lubi,zagadki).
+lubi(zagadki,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(zagadki)],Stack),
+	pytaj([tak],zagadki,Stack,Stack1),
+	lubi_grac_w(przygodowe,Stack1,Stack2),
+	pytaj([kryminalistyka],studia,Stack2,Stack3),
+	pytaj([czytanie_w_myslach,niewidzialnosc,podroz_w_czasie],moc,Stack3,ResultStack),
+	pamietaj(lubi,zagadki,ResultStack).
 	
-lubi(podroze) :-
-	pytaj([tak],podroze),
-	pytaj([gory],wakacje),
-	pytaj([latanie,podroz_w_czasie],moc),
-	pamietaj(lubi,podroze).
+lubi(podroze,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(podroze)],Stack),
+	pytaj([tak],podroze,Stack,Stack1),
+	pytaj([gory],wakacje,Stack1,Stack2),
+	pytaj([latanie,podroz_w_czasie],moc,Stack2,ResultStack),
+	pamietaj(lubi,podroze,ResultStack).
 	
-lubi(horrory) :-
-	lubi_grac_w(horrory),
-	typ(pesymista),
-	pamietaj(lubi,horrory).
+lubi(horrory,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(horrory)],Stack),
+	lubi_grac_w(horrory,Stack,Stack1),
+	typ(pesymista,Stack1,ResultStack),
+	pamietaj(lubi,horrory,ResultStack).
 		
-lubi(biografia) :-
-	lubi(historia),
-	pytaj([tak],postac_historyczna),
-	pamietaj(lubi,biografia).
+lubi(biografia,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(biografia)],Stack),
+	lubi(historia,Stack,Stack1),
+	pytaj([tak],postac_historyczna,Stack1,ResultStack),
+	pamietaj(lubi,biografia,ResultStack).
 
 
-lubi(lekkie_pozycje) :-
-    (pytaj([okazjonalnie,rzadko],jak_czesto);
-    czyta(rzadko);
-    pytaj([seriale],oglada)),
-    pytaj([impulsywny],jest),
-    (pytaj([tak],podroze);
-    pytaj([przygodowe,akcji,familijne],filmy)),
-    pamietaj(lubi,lekkie_pozycje).
+lubi(lekkie_pozycje,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(lekkie_pozycje)],Stack),
+    (pytaj([okazjonalnie,rzadko],jak_czesto,Stack,Stack1);
+    czyta(rzadko,Stack,Stack1);
+    pytaj([seriale],oglada,Stack,Stack1)),
+    pytaj([impulsywny],jest,Stack1,Stack2),
+    (pytaj([tak],podroze,Stack2,ResultStack);
+    pytaj([przygodowe,akcji,familijne],filmy,Stack2,ResultStack)),
+    pamietaj(lubi,lekkie_pozycje,ResultStack).
         
     
-lubi(ciezkie_pozycje) :-
-    (pytaj([codziennie],jak_czesto);
-    czyta(czesto);
-    pytaj([filmy],oglada)),
-    (pytaj([tak],wyobraznia);
-    pytaj([psychologiczne,thrillery,dramaty],filmy)),
-    pytaj([opanowany],jest),
-    pamietaj(lubi,ciezkie_pozycje).
+lubi(ciezkie_pozycje,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(ciezkie_pozycje)],Stack),
+    (pytaj([codziennie],jak_czesto,Stack,Stack1);
+    czyta(czesto,Stack,Stack1);
+    pytaj([filmy],oglada,Stack,Stack1)),
+    (pytaj([tak],wyobraznia,Stack1,Stack2);
+    pytaj([psychologiczne,thrillery,dramaty],filmy,Stack1,Stack2)),
+    pytaj([opanowany],jest,Stack2,ResultStack),
+    pamietaj(lubi,ciezkie_pozycje,ResultStack).
 
-lubi(naukowe) :-
-    pytaj([informatyka,fizyka],studia),
-    pytaj([tak],nauka),
-    pytaj([tak],zycie_pozaziemskie),
-    pytaj([czytanie_w_myslach,niewidzialnosc,podroz_w_czasie],moc),
-    pamietaj(lubi,naukowe).
+lubi(naukowe,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(naukowe)],Stack),
+    pytaj([informatyka,fizyka],studia,Stack,Stack1),
+    pytaj([tak],nauka,Stack1,Stack2),
+    pytaj([tak],zycie_pozaziemskie,Stack2,Stack3),
+    pytaj([czytanie_w_myslach,niewidzialnosc,podroz_w_czasie],moc,Stack3,ResultStack),
+    pamietaj(lubi,naukowe,ResultStack).
 
         	
-lubi(romans) :-
-    pytaj([kolacja,film_w_domu],urodziny_partnera),
-    pytaj([rpg,przygodowe],gry_komputerowe),
-    pytaj([introwertyk,melancholik],charakter),
-    pytaj([pelna],szklanka),
-    pytaj([daje_pieniadze,kupuje_mu_jedzenie],bezdomny),
-    pamietaj(lubi,romans).
+lubi(romans,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(romans)],Stack),
+    pytaj([kolacja,film_w_domu],urodziny_partnera,Stack,Stack1),
+    pytaj([rpg,przygodowe],gry_komputerowe,Stack1,Stack2),
+    pytaj([introwertyk,melancholik],charakter,Stack2,Stack3),
+    pytaj([pelna],szklanka,Stack3,Stack4),
+    pytaj([daje_pieniadze,kupuje_mu_jedzenie],bezdomny,Stack4,ResultStack),
+    pamietaj(lubi,romans,ResultStack).
 
-lubi(historia) :-
-    (pytaj([tak],postac_historyczna),
-    pytaj([shootery,strategie],gry_komputerowe),
-    pytaj([gory,zwiedzanie_zabytkow],wakacje));
-    pytaj([tak],historia),
-    pytaj([historia],studia),
-    pytaj([niewidzialnosc,podroz_w_czasie],moc),
-    pamietaj(lubi,historia).        
+lubi(historia,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(historia)],Stack),
+    (pytaj([tak],postac_historyczna,Stack,Stack1),
+    pytaj([shootery,strategie],gry_komputerowe,Stack1,Stack2),
+    pytaj([gory,zwiedzanie_zabytkow],wakacje,Stack2,Stack3));
+%przekminiæ rozwi¹zanie
+    pytaj([tak],historia,Stack,Stack1),
+    pytaj([historia],studia,Stack1,Stack2),
+    pytaj([niewidzialnosc,podroz_w_czasie],moc,Stack2,ResultStack),
+    pamietaj(lubi,historia,ResultStack).        
 
-lubi(podroze) :-
-    pytaj([tak],podroze),
-    pamietaj(lubi,podroze).
+lubi(podroze,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(podroze)],Stack),
+    pytaj([tak],podroze,Stack,ResultStack),
+    pamietaj(lubi,podroze,ResultStack).
 	
-typ(optymista) :-
-	pytaj([pelna],szklanka),
-	pamietaj(typ,optymista).
+typ(optymista,InitialStack,ResultStack) :-
+	append(InitialStack,[typ(optymista)],Stack),
+	pytaj([pelna],szklanka,Stack,ResultStack),
+	pamietaj(typ,optymista,ResultStack).
 	
-typ(pesymista) :-
-	pytaj([pusta],szklanka),
-	pamietaj(typ,pesymista).
+typ(pesymista,InitialStack,ResultStack) :-
+	append(InitialStack,[typ(pesymista)],Stack),
+	pytaj([pusta],szklanka,Stack,ResultStack),
+	pamietaj(typ,pesymista,ResultStack).
 	
-lubi_grac_w(X) :-
-    pytaj([X],gry_komputerowe).
+lubi_grac_w(X,InitialStack,ResultStack) :-
+	append(InitialStack,[lubi(biografia)],Stack),
+    pytaj([X],gry_komputerowe,Stack,ResultStack).
     
 
-% Funkcje pomocnicze
-pamietaj(X, Y) :-
+% Funkcje pomocnicze    
+pamietaj(X, Y, LIST) :-
+    write(X),write(' -> '),write(Y),write(' : '),write(LIST),nl,
     assertz(memory(X, Y)).
+        
     
 sprawdz([],_) :- false.    
 sprawdz([X|Rest],Type) :- memory(Type,X);sprawdz(Rest,Type).
 
-pytaj(Answer,Type) :-
+pytaj(Answer,Type,Stack,NewStack) :-
 	sprawdz(Answer,Type).
-	
-pytaj(Answer,Type) :-
+
+pytaj(Answer,Type, Stack, NewStack) :-
 	question(Question,Answers,Type),
 	(not(sprawdz(Answers, Type)),
-	uzyskaj_poprawna_odpowiedz(Question,Answers,Reply),
-	pamietaj(Type,Reply),
-	member(Reply, Answer)).
-	
-uzyskaj_poprawna_odpowiedz(Question,Answers,Reply):-
+	uzyskaj_poprawna_odpowiedz(Question,Answers,Reply,Stack),
+	pamietaj(Type,Reply,Stack),
+	member(Reply, Answer)),
+	append(Stack,[[Question , Reply]],NewStack).
+			
+uzyskaj_poprawna_odpowiedz(Question,Answers,Reply,Stack):-
 	repeat,
 	write(Question),write(Answers),write('?: '),nl,
 	readln([Reply]),
 	what(Reply),
+	why(Reply,Stack),
 	(member(Reply,Answers)),!.
 
 what(what) :-
@@ -214,6 +237,13 @@ what(what) :-
 	write(A),write(' '),write(B),nl.
 
 what(_) :- true.
+
+why(why,Stack) :-
+	nl,write('Rules: '),nl,
+	write(Stack),
+	nl,nl.
+
+why(_,_) :- true.
 
 % Czyszczenie zapisanych danych
 wyczysc_fakty :-
